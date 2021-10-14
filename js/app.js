77aa63f5eea49f91c221acbd659b40df8e97e512
gentlevulture <novemberboughs@gmail.com>
2021-10-14 14:52:33 -0700

    initialPseudocode

--- js/app.js
+++ js/app.js
@@ -1,26 +1,18 @@
-//  
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+// Set intitial variables
+// Age will begin at 5 with maxAge of 29
+// Also set variables for hunger and fitness levels
+// Create dev elements that hold weight and health of dog
+// Create division elements for 3 separate buttons: feed, hunt and, fire
+// Button click functions will call functions as described
+// Food button function will increase pet Increase pet weight wait and health
+// Food must be cooked in fire to feed Camp Dog. After all, he can hunt for raw meat by himself
+// Fire button function will in decrease pet weight but increase pet health
+// Firewood must be collected during hunts
+// Hunt button will decrease pet health and decrease pet weight
+// Hunt success will be determined by Camp Dogs current health and weight over timer
+// Hunt function will also contain chance conditionals to obtain RawMeat and FireWood
+// Create function for checking pet status
+// Create timer function that lowers Health and Wight over time
+// Status check function will contain conditional so if Weight or Health of Camp Dog are lower than set set perameter, Camp Dog will run away
+// Camper is left alone 
+// Camp Dog has no name
