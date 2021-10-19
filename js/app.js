4c8baaa4a5908d4a38241b3d76d6925dc61e166a
gentlevulture <novemberboughs@gmail.com>
2021-10-18 21:55:51 -0700

    depleteMeter

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 //// two variables representing meters for nourishment and warmth levels
 //// start counter at 0 to begin measuring time Camp Dog stays at camp
 //// creat timer to measure time passed
-// create button click elements that will call functions to increase nourish and warmth
-// Food button function will increase pet nourishment
-// Fire button function will increase pet warmth
+//// create button click elements that will call functions to increase nourish and warmth
+//// Food button function will increase pet nourishment
+//// Fire button function will increase pet warmth
 // Create timer function that lowers warmth and nourishment over time
 // Status check function will contain conditional so if Weight or Health of Camp Dog are lower than set set perameter, Camp Dog will run away and console disappears
 // GameOver Screen
@@ -14,30 +14,42 @@
 // Camper is left alone 
 // Camp Dog has no name
 
-let nourish = 10;
-let warmth = 10;
+let nourish = 10
+let warmth = 10
 
-let parSecs = 0, clock;
+let parSecs = 0, clock
 
 
-clock = setInterval(elapsedTime,1000);
+clock = setInterval(elapsedTime,1000)
 
 function elapsedTime() {
-    parSecs++;
+    parSecs++
+    meter()
     console.log(parSecs) 
     console.log(`warmth: ${warmth}, nourish: ${nourish}`)  
 } 
 
 document.getElementById("bone").onclick = function feed() {
     if (nourish < 15) {
-        nourish++;
+        nourish++
         console.log("CampDog fed")
     }
 }
 
 document.getElementById("fire").onclick = function heal() {
     if (warmth < 15) {
-        warmth++;
+        warmth++
         console.log("CampDog warmed up")
     }
+}
+
+function meter() {
+    if (whichMeter() === 0) {
+        nourish--
+    } else if (whichMeter() === 1) {
+        warmth--
+    }
+}
+function whichMeter() {
+    return Math.floor(Math.random()*2)
 }
